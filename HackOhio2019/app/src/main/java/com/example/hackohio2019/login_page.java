91f73e1a258929af12f05656636dc03aaff5947e
Katie Nichols <krnichols99@gmail.com>
2019-11-03 03:41:02 -0500

    add functionality to login button (only changes pages, doesn't actually check any values or log's in)

--- HackOhio2019/app/src/main/java/com/example/hackohio2019/login_page.java
+++ HackOhio2019/app/src/main/java/com/example/hackohio2019/login_page.java
@@ -2,7 +2,10 @@ package com.example.hackohio2019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login_page extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class login_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+
+        Button btnLogin = (Button)findViewById((R.id.btnLogin));
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent loginIntent = new Intent(getApplicationContext(),home_page.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
+
+
+//        Button txtSignUp = (Button)findViewById((R.id.txtSignUp));
+//        txtSignUp.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                Intent signupIntent = new Intent(getApplicationContext(),signup_page.class);
+//                startActivity(signupIntent);
+//            }
+//        });

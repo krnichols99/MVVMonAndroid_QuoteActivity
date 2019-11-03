8085ea6360fabda7670be1107d73702dc49fe25e
Katie Nichols <krnichols99@gmail.com>
2019-11-03 04:44:19 -0500

    attempting to add click functionality to signup text view (should only change pages)

--- HackOhio2019/app/src/main/java/com/example/hackohio2019/login_page.java
+++ HackOhio2019/app/src/main/java/com/example/hackohio2019/login_page.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class login_page extends AppCompatActivity {
 
@@ -23,15 +26,16 @@ public class login_page extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         });
+
+        TextView txtSignUp = (TextView) findViewById((R.id.txtSignUp));
+        txtSignUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent signupIntent = new Intent(getApplicationContext(),signup_page.class);
+                startActivity(signupIntent);
+            }
+        });
     }
 }
 
 
-//        Button txtSignUp = (Button)findViewById((R.id.txtSignUp));
-//        txtSignUp.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                Intent signupIntent = new Intent(getApplicationContext(),signup_page.class);
-//                startActivity(signupIntent);
-//            }
-//        });
